eae0e95b9d93a10fd8c04cac310617da50fa7946
Christian Stjernberg <christian@stjernberg.com>
2023-05-31 21:52:33 +0200

    Complete script
    maybe.....

--- dategenerator.py
+++ dategenerator.py
@@ -1,23 +1,38 @@
-import datetime
+import datetime, subprocess
 from dateutil.relativedelta import relativedelta
 
-input = "011110010001100000010001101111110110000110000011110110011010001100000010001101100000110000110000110011110011011111100000011111101111100110000110000110011110011010001101100010001101100000110000110000110011110011010001101100010001101100000110000110000110011011110010001100100010001101111110111110111110011110000000000000001000000000000000000000000000000000000"
+input = ""
+
+# Input is string with 51 by 7 zeroes or ones
+with open("input", "r") as input_file:
+    input = input_file.read().replace("\n", "")
 
 bitlist = []
 counter = 0
 
-# Iterate through bitmap and generate one-dimensional list
+# Iterate through bitmap and generate list of index of 1-bits
 for i in range(0,51):
     for j in range(0,7):
         if input[i+j*51] == '1':
             bitlist.append(counter)
         counter += 1
-        
+
 
 yearago = datetime.date.today() - relativedelta(years=1)
-yearago_weekday = yearago.weekday() + 1 % 7 
+yearago_weekday = yearago.weekday() + 1 % 7
 last_sunday_yearago = yearago - relativedelta(days=yearago_weekday)
 
+datelist = [str(last_sunday_yearago + relativedelta(days=number)) for number in bitlist]
 
-for number in bitlist:
-    print(last_sunday_yearago + relativedelta(days=number))
+subprocess.run(["git", "checkout", "--orphan", "fake-commits"])
+
+for date in datelist:
+    subprocess.run(["git", "commit", "--allow-empty", "--date", date, "-m", date])
+    subprocess.run(["git", "commit", "--allow-empty", "--date", date, "-m", date])
+    subprocess.run(["git", "commit", "--allow-empty", "--date", date, "-m", date])
+    subprocess.run(["git", "commit", "--allow-empty", "--date", date, "-m", date])
+
+
+subprocess.run(["git", "checkout", "main"])
+subprocess.run(["git", "merge", "--allow-unrelated-histories", "fake-commits"])
+
